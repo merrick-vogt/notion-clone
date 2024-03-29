4eaa5613634a7ee1d0741f3aca9f7c5927ff545d
Nathan Vogt <merrick.vogt@gmail.com>
2024-03-28 20:49:47 -0400

    added create Page and fixed dependency issues

--- ztm-notion-clone/src/App.tsx
+++ ztm-notion-clone/src/App.tsx
@@ -1,15 +1,17 @@
 import './App.css'
-import { Cover } from './Page/Cover'
 import { Page } from './Page/Page'
+import { AppStateProvider } from './state/AppStateContext'
+import { createPage } from './utils/createPage'
 
+
+
+const initialState = createPage();
 
 function App() {
-  
-
   return (
-    <>
+    <AppStateProvider initialState={initialState}>
       <Page/>
-    </>
+    </AppStateProvider>
   )
 }
 
